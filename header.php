59e9dc77948d69a1d7dda80e2188a33125873694
rajat.gupta <rajat.guptakr2@gmail.com>
2017-11-07 07:58:37 +0530

    yes

---
+++ header.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Grocery </title>
+
+
+<!--css links-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<link rel="stylesheet" href="assets/css/developer.css" type="text/css">
+<link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="assets/css/multiple-select.css" type="text/css">
+<link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+ 
+  
+</head>
+ <body>
+<main>
+<!--header section start-->
+ <header>
+	<div class="container">
+		<div class="row">
+			<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"><img class="mylogo" src="assets/images/allaboutgrossery-logo.png"></a>
+    </div>
+    <div class="collapse navbar-collapse " id="myNavbar">
+      <ul class="nav navbar-nav grocerymenu menu-black navbar-right">
+        <li><a href="javascript:void(0)">Home</a></li>
+        <li><a href="javascript:void(0)">About Us</a></li>
+        <li><a href="javascript:void(0)">Contact Us</a></li>
+        <li><a href="javascript:void(0)">Login</a></li>
+		 	
+      </ul>
+    </div>
+  </div>
+</nav>
+		</div>
+	</div>	
+	 
+  </header>  
